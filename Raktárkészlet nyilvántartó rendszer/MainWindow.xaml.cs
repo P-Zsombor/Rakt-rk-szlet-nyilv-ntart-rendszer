c7a7bbe079830bbd172563df51bb1aff8ebfb8a6
David Watson <jenokiss187@gmail.com>
2025-05-25 18:24:46 +0200

    Add project files.

---
+++ Raktárkészlet nyilvántartó rendszer/MainWindow.xaml.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Raktárkészlet_nyilvántartó_rendszer
+{
+    public partial class MainWindow : Window
+    {
+        private ServerConnection con;
+
+        Border cb;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            con = new ServerConnection();
+            clearContent();
+        }
+
+        private async void ClickAdd(object sender, EventArgs e)
+        {
+            try
+            {
+                bool success = await con.CreateProduct(
+    nameInput.Text,
+    typeInput.Text,
+    Convert.ToInt32(priceInput.Text)
+);
+
+
+
+                if (success)
+                {
+                    MessageBox.Show("Termék sikeresen hozzáadva");
+                    ClearInputs();
+                }
+                else
+                {
+                    MessageBox.Show("Hiba történt a termék hozzáadásakor");
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("error:" + err.Message);
+            }
+
+
+
+        }
+
+        private async void ClickShowAll(object sender, EventArgs e)
+        {
+            ClearDisplay();
+
+            List<JsonData> products = await con.GetAllProducts();
+
+
+            if (products.Count == 0)
+            {
+                MessageBox.Show("Nincsenek termékek.");
+                return;
+            }
+
+            foreach (var p in products)
+            {
+
+                cb = new Border
+                {
+                    Width = 240,
+                    Height = 170,
+                    Margin = new Thickness(10),
+                    Background = new SolidColorBrush(Color.FromRgb(0, 0, 75)),
+                    CornerRadius = new CornerRadius(20),
+
+                    Child = new TextBlock
+                    {
+                        Text = $"ID: {p.id}\nNév: {p.name}\nTípus: {p.type}\nÁr: {p.price}",
+                        TextWrapping = TextWrapping.Wrap,
+                        TextAlignment = TextAlignment.Center,
+                        Foreground = Brushes.White,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        FontSize = 16,
+                    },
+                };
+                Everything.Children.Add(cb);
+            }
+        }
+
+        private async void ClickShowById(object sender, EventArgs e)
+        {
+            ClearDisplay();
+
+            if (!int.TryParse(IdInput.Text, out int id))
+            {
+                MessageBox.Show("Érvénytelen azonosító.");
+                return;
+            }
+
+            JsonData product = await con.GetProductById(id.ToString());
+            if (product != null)
+            {
+
+                var tb = new TextBlock
+                {
+                    Text = $"> ID: {product.id} Név: {product.name} Típus: {product.type} Ár: {product.price}",
+                    TextWrapping = TextWrapping.Wrap,
+                    FontSize = 24,
+                    TextAlignment = TextAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    Margin = new Thickness(20, 30, 0, 0)
+                };
+                
+                SearchRes.Children.Add(tb);
+            }
+            else
+            {
+                MessageBox.Show("Nem található ilyen azonosítójú termék.");
+            }
+        }
+
+        private async void ClickEdit(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(IdInput.Text.Trim(), out int id))
+            {
+                MessageBox.Show("Érvénytelen termékazonosító.");
+                return;
+            }
+
+            string newName = nameInput.Text.Trim();
+            string newType = typeInput.Text.Trim();
+            if (!int.TryParse(priceInput.Text.Trim(), out int newPrice))
+            {
+                MessageBox.Show("Érvénytelen ár.");
+                return;
+            }
+
+            bool success = await con.EditProduct(id, newName, newType, newPrice);
+            if (success)
+            {
+                MessageBox.Show("Termék sikeresen módosítva.");
+                ClearDisplay();
+                ClickShowAll(null, null);
+            }
+            else
+            {
+                MessageBox.Show("Hiba történt a termék módosításakor.");
+            }
+        }
+
+        private async void ClickDelete(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(IdInput.Text.Trim(), out int id))
+            {
+                MessageBox.Show("Érvénytelen termékazonosító.");
+                return;
+            }
+
+            bool success = await con.DeleteProduct(id);
+            if (success)
+            {
+                MessageBox.Show("Termék sikeresen törölve.");
+                ClearDisplay();
+                ClickShowAll(null, null);
+            }
+            else
+            {
+                MessageBox.Show("Hiba történt a termék törlésekor.");
+            }
+        }
+
+        private async void ClickShowByName(object sender, EventArgs e)
+        {
+            ClearDisplay();
+
+            string name = nameInput.Text.Trim();
+            if (string.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("Érvénytelen név.");
+                return;
+            }
+
+            JsonData product = await con.GetProductByName(name);
+            if (product != null)
+            {
+                var tb = new TextBlock
+                {
+                    Text = $"> ID: {product.id} — Név: {product.name} — Típus: {product.type} — Ár: {product.price}",
+                    TextWrapping = TextWrapping.Wrap,
+                    FontSize = 24,
+                    TextAlignment = TextAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    Margin = new Thickness(20,30,0,0)
+                    
+                };
+                SearchRes.Children.Add(tb);
+            }
+            else
+            {
+                MessageBox.Show("Nem található ilyen nevű termék.");
+            }
+        }
+
+        private void ClearDisplay()
+        {
+            SearchRes.Children.Clear();
+        }
+
+        private void ClearInputs()
+        {
+            nameInput.Text = string.Empty;
+            typeInput.Text = string.Empty;
+            priceInput.Text = string.Empty;
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            if (tb != null && tb.Text == (string)tb.Tag)
+            {
+                tb.Text = "";
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            if (tb != null && string.IsNullOrWhiteSpace(tb.Text))
+            {
+                tb.Text = (string)tb.Tag;
+            }
+        }
+
+        private void Items_menaggement_Click(object sender, RoutedEventArgs e)
+        {
+            item_container.Visibility = Visibility.Visible;
+            warehouse_container.Visibility = Visibility.Hidden;
+
+            Itemadd.Visibility = Visibility.Visible;
+            ShowbynameB.Visibility = Visibility.Visible;
+            ShowallB.Visibility = Visibility.Visible;
+            showbyidB.Visibility = Visibility.Visible;
+            itemedit.Visibility = Visibility.Visible;
+            itemdelete.Visibility = Visibility.Visible;
+
+            WH_add.Visibility = Visibility.Collapsed;
+            WH_searchbyID.Visibility = Visibility.Collapsed;
+            WH_ShowAll.Visibility = Visibility.Collapsed;
+        }
+
+        private void Warehouse_menaggement_Click(object sender, RoutedEventArgs e)
+        {
+            item_container.Visibility = Visibility.Hidden;
+            warehouse_container.Visibility = Visibility.Visible;
+
+            Itemadd.Visibility = Visibility.Collapsed;
+            ShowbynameB.Visibility = Visibility.Collapsed;
+            ShowallB.Visibility = Visibility.Collapsed;
+            showbyidB.Visibility = Visibility.Collapsed;
+            itemedit.Visibility = Visibility.Collapsed;
+            itemdelete.Visibility = Visibility.Collapsed;
+
+            WH_add.Visibility = Visibility.Visible;
+            WH_searchbyID.Visibility = Visibility.Visible;
+            WH_ShowAll.Visibility = Visibility.Visible;
+        }
+
+        void clearContent()
+        {
+            item_container.Visibility = Visibility.Hidden;
+            warehouse_container.Visibility = Visibility.Hidden;
+            Itemadd.Visibility = Visibility.Collapsed;
+            ShowbynameB.Visibility = Visibility.Collapsed;
+            ShowallB.Visibility = Visibility.Collapsed;
+            showbyidB.Visibility = Visibility.Collapsed;
+            itemedit.Visibility = Visibility.Collapsed;
+            itemdelete.Visibility = Visibility.Collapsed;
+            WH_add.Visibility = Visibility.Collapsed;
+            WH_searchbyID.Visibility = Visibility.Collapsed;
+            WH_ShowAll.Visibility = Visibility.Collapsed;
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            clearContent();
+        }
+
+        private async void WH_add_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (WH_name.Text.Length == 0 || WH_location.Text.Length == 0 || WH_capacity.Text.Length == 0 || WH_manager_name.Text.Length == 0)
+                {
+                    MessageBox.Show("nem jók a mezők");
+                    return;
+                }
+                int temp;
+                if (!int.TryParse(WH_capacity.Text, out temp))
+                {
+                    MessageBox.Show("csak számot lehet megadni");
+                    return;
+                }
+
+                bool success = await con.CreateWarehouse(
+                    WH_name.Text,
+                    WH_location.Text,
+                    WH_capacity.Text,
+                    WH_manager_name.Text,
+                    WH_notes.Text
+                );
+
+                if (success)
+                {
+                    MessageBox.Show("Sikeres létrehozás");
+                }
+                else
+                {
+                    MessageBox.Show("Sikertelen létrehozás");
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Hiba: " + err.Message);
+            }
+        }
+
+        private async void WH_searchbyID_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!int.TryParse(WH_id.Text, out int id))
+                {
+                    MessageBox.Show("Érvénytelen ID!");
+                    return;
+                }
+
+                Warehouse warehouse = await con.GetWarehouseById(id);
+
+                if (warehouse == null)
+                {
+                    MessageBox.Show("Nem található ilyen ID-jű raktár.");
+                    return;
+                }
+
+                cb = new Border
+                {
+                    Margin = new Thickness(10),
+                    Background = new SolidColorBrush(Color.FromRgb(0, 0, 75)),
+                    CornerRadius = new CornerRadius(20),
+                    Height = 300,
+
+                    Child = new TextBlock
+                    {
+                        Text = $"ID: {warehouse.id}\nNév: {warehouse.name}\nHelye: {warehouse.location}\nKapacitás: {warehouse.capacity}\nVezető név: {warehouse.manager_name}\nEgyéb megjegyzések: {warehouse.notes}",
+                        TextWrapping = TextWrapping.Wrap,
+                        TextAlignment = TextAlignment.Center,
+                        Foreground = Brushes.White,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        FontSize = 16,
+                    },
+                };
+
+                WH_searchResult.Children.Clear();
+                WH_searchResult.Children.Add(cb);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Hiba: " + err.Message);
+            }
+        }
+
+
+
+        private async void WH_ShowAll_Click(object sender, RoutedEventArgs e)
+        {
+            WHEverything.Children.Clear();
+            List <Warehouse> WH_all = await con.GetAllWarehouses();
+
+            if (WH_all.Count == 0)
+            {
+                MessageBox.Show("Nincs adatod");
+            }
+
+            try
+            {
+                foreach (var WH in WH_all)
+                {
+
+                    cb = new Border
+                    {
+                        Width = 240,
+                        Height = 170,
+                        Margin = new Thickness(10),
+                        Background = new SolidColorBrush(Color.FromRgb(0, 0, 75)),
+                        CornerRadius = new CornerRadius(20),
+
+                        Child = new TextBlock
+                        {
+                            Text = $"ID: {WH.id}\nNév: {WH.name}\nVezető név: {WH.manager_name}",
+                            TextWrapping = TextWrapping.Wrap,
+                            TextAlignment = TextAlignment.Center,
+                            Foreground = Brushes.White,
+                            VerticalAlignment = VerticalAlignment.Center,
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            FontSize = 16,
+                        },
+                    };
+                    WHEverything.Children.Add(cb);
+
+                }
+            }
+            catch (Exception err)
+            {
+
+                MessageBox.Show("error: " + err.Message);
+            }
+
+          
+        }
+    }
+}
---
+++ Raktárkészlet nyilvántartó rendszer/ServerConnection.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace Raktárkészlet_nyilvántartó_rendszer
+{
+    internal class ServerConnection
+    {
+        public string BaseUrl = "http://localhost:3000";
+        HttpClient client = new HttpClient();
+        public ServerConnection() 
+        {
+
+        }
+        public async Task<bool> CreateProduct(string name, string type, int price)
+        {
+            string url = BaseUrl + "/productcreate";
+
+            try
+            {
+                var jsonInfo = new
+                {
+                    createProductName = name,
+                    createProductType = type,
+                    createProductPrice = price
+                };
+
+                string jsonString = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                response.EnsureSuccessStatusCode();
+
+                string result = await response.Content.ReadAsStringAsync();
+                JsonData responseData = JsonConvert.DeserializeObject<JsonData>(result);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hiba a termék létrehozásakor: " + e.Message);
+                return false;
+            }
+        }
+
+        public async Task<List<JsonData>> GetAllProducts()
+        {
+            string url = BaseUrl + "/products";
+            List<JsonData> products = new List<JsonData>();
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string result = await response.Content.ReadAsStringAsync();
+                products = JsonConvert.DeserializeObject<List<JsonData>>(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hiba a termékek lekérdezésekor: " + e.Message);
+            }
+
+            return products;
+        }
+
+        public async Task<JsonData> GetProductById(string id)
+        {
+            string url = BaseUrl + "/productsid";
+
+            try
+            {
+                var jsonInfo = new { id = id };
+                string jsonString = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                response.EnsureSuccessStatusCode();
+
+                string result = await response.Content.ReadAsStringAsync();
+                JsonData product = JsonConvert.DeserializeObject<JsonData>(result);
+
+                return product;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hiba a termék lekérdezésekor: " + e.Message);
+                return null;
+            }
+        }
+
+        //-----------------------------------------------------
+
+        public async Task<bool> EditProduct(int id, string name, string type, int price)
+        {
+            string url = BaseUrl + "/update";
+
+            try
+            {
+                var jsonInfo = new
+                {
+                    id = id,
+                    name = name,
+                    type = type,
+                    price = price
+                };
+                string jsonString = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PutAsync(url, content);
+                response.EnsureSuccessStatusCode();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hiba a termék módosításakor: " + e.Message);
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteProduct(int id)
+        {
+            string url = BaseUrl + "/deletProduct";
+
+            try
+            {
+                var jsonInfo = new { id = id };
+                string jsonString = JsonConvert.SerializeObject(jsonInfo);
+                var request = new HttpRequestMessage(HttpMethod.Delete, url)
+                {
+                    Content = new StringContent(jsonString, Encoding.UTF8, "application/json")
+                };
+
+                HttpResponseMessage response = await client.SendAsync(request);
+                Console.WriteLine("Sikeres törlés");
+                response.EnsureSuccessStatusCode();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hiba a termék törlésekor: " + e.Message);
+                return false;
+            }
+        }
+
+        public async Task<JsonData> GetProductByName(string name)
+        {
+            string url = BaseUrl + "/productsearchname";
+
+            try
+            {
+                var jsonInfo = new { name = name };
+                string jsonString = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                response.EnsureSuccessStatusCode();
+
+                string result = await response.Content.ReadAsStringAsync();
+                JsonData product = JsonConvert.DeserializeObject<JsonData>(result);
+
+                return product;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hiba a termék lekérdezésekor: " + e.Message);
+                return null;
+            }
+        }
+
+        //-----------------------------------------------------
+
+        public async Task<bool> CreateWarehouse(string name, string location, string capacity, string managerName, string notes)
+        {
+            string url = BaseUrl + "/warehouses";
+
+            try
+            {
+                var warehouseInfo = new
+                {
+                    name = name,
+                    location = location,
+                    capacity = capacity,
+                    manager_name = managerName,
+                    notes = notes
+                };
+
+                string jsonString = JsonConvert.SerializeObject(warehouseInfo);
+                HttpContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                response.EnsureSuccessStatusCode();
+
+                string result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Raktár sikeresen létrehozva: " + result);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hiba a raktár létrehozásakor: " + e.Message);
+                return false;
+            }
+        }
+
+        public async Task<List<Warehouse>> GetAllWarehouses()
+        {
+            string url = BaseUrl + "/warehouses";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string result = await response.Content.ReadAsStringAsync();
+                var warehouses = JsonConvert.DeserializeObject<List<Warehouse>>(result);
+
+                return warehouses;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hiba a raktárak lekérdezésekor: " + e.Message);
+                return new List<Warehouse>();
+            }
+        }
+
+        public async Task<Warehouse> GetWarehouseById(int id)
+        {
+            string url = $"{BaseUrl}/warehouses/{id}";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string result = await response.Content.ReadAsStringAsync();
+                var warehouse = JsonConvert.DeserializeObject<Warehouse>(result);
+
+                return warehouse;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hiba a raktár lekérdezésekor: " + e.Message);
+                return null;
+            }
+        }
+
+    }
+
+    public class Warehouse
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string location { get; set; }
+        public int capacity { get; set; }
+        public string manager_name { get; set; }
+        public string notes { get; set; }
+    }
+}
+
